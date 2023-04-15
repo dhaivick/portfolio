5b8c909806295d898c131b85026d59c92146c6ba
Dhaivick Jasumal <dhaivick@elula.tech>
2023-04-15 14:59:06 +0530

    Added footer styles

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,9 @@
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-  return <footer className={styles.footer}>Dhaivick Jasumal © 2023.</footer>;
+  return (
+    <footer className={styles.footer}>
+      Dhaivick Jasumal © {new Date().getFullYear()}.
+    </footer>
+  );
 }
